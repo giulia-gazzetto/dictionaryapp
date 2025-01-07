dd651093df83db8b19a55f14c676e02e17c120eb
gio-gionni <g.gazzetto@gmail.com>
2025-01-07 17:26:06 +0100

    deployed site and added basic links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">Dictionary</header>
+      <footer>
+        Coded by <a href="https://github.com/gio-gionni">Giulia</a> opens
+        sourced on{" "}
+        <a href="https://github.com/gio-gionni/dictionaryapp">Github</a> and
+        hosted on{" "}
+        <a href="https://dictionaryappbygiulia.netlify.app/">Netlify</a>
+      </footer>
     </div>
   );
 }
