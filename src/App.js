5d2146ef9e343f71a12879a6eea194ffd991e5c8
gio-gionni <g.gazzetto@gmail.com>
2025-01-07 18:00:43 +0100

    added search engine form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">Dictionary</header>
+      <div>
+        <Dictionary />
+      </div>
       <footer>
         Coded by <a href="https://github.com/gio-gionni">Giulia</a> opens
         sourced on{" "}
